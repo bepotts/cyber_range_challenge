9783a6719c31c9817f0987285ac99063bd68bc6e
Brandon Potts <ObeliskIsHuge@gmail.com>
2017-11-27 22:14:17 -0800

    created test for returning a JSON file

--- test/testServer.js
+++ test/testServer.js
@@ -2,7 +2,10 @@
  * Test routes
  */
 
+import assert from 'assert';
+
 const request = require('supertest');
+const check = require('check-types');
 
 
 describe('loading express', () => {
@@ -13,9 +16,7 @@ describe('loading express', () => {
   });
 
   afterEach(() => {
-    console.log('server is about to be called');
     server.close();
-    console.log('server was just called');
   });
 
   after(() => {
@@ -23,12 +24,31 @@ describe('loading express', () => {
   });
 
   /* Test index route */
-  describe('GET /', () => {
+  describe('Testing root route. GET /', () => {
     it('responded with a html page', (done) => {
       request(server)
         .get('/')
         .expect('Content-Type', 'text/html; charset=UTF-8')
         .expect(200, done);
+    });
+  });
+
+  /* Test return of JSON file with Fahrenheit */
+  describe('Testing JSON serves. GET /locations/:zipCode', () =>{
+    it('returned a JSON file', (done) => {
+      request(server)
+        .get('/locations/24060')
+        .expect('Content-Type', 'application/json; charset=utf-8')
+        .expect(200)
+        .end((err, res) => {
+          if(err) {
+            return done(err);
+          }
+
+          assert.equal(res.body.scale, 'Fahrenheit');
+          check.assert.match(res.body.temperature, '\\d+');
+          done();
+        })
     });
   });
 });
